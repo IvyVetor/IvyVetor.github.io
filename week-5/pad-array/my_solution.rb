0f9d5b7e9bda35c3a66c4f30a92db3fb43af46b2
Ivy Vetor <ivyvetor@gmail.com>
2016-01-27 21:59:24 -0600

    Add files for post pairing 5.2

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -2,6 +2,7 @@
 
 # I worked on this challenge [by myself, with: ]
 # => With Sepand Assadi
+
 # I spent [] hours on this challenge.
 
 
@@ -13,20 +14,44 @@
 
 # 0. Pseudocode
 
-# What is the input? an array and the minimum size
-# What is the output? (i.e. What should the code return?)
+# What is the input? array, integer, value
+# What is the output? (i.e. What should the code return?) array
 # What are the steps needed to solve the problem?
+# IF user minumum size is equal or less than array length
+# return the array
+# ELSE
+# Create a variable to store the difference between user minumum size and array length
+# Create temporary array with a size of "difference variable"
+# Create new array and store both user array and temp array
+# Replace user array with new array
 
 
 # 1. Initial Solution
+
 def pad!(array, min_size, value = nil) #destructive
-  # Your code here
+
+  if min_size <= array.length
+  array
+  else
+  diff_size = min_size - array.length
+  temp_array = [value] * diff_size
+  new_array = array + temp_array
+  end
+  return array
 end
+
 
 def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
+  new_array = array.clone
+  if min_size <= array.length
+    return new_array
+  else
+    diff_size = min_size - array.length
+    temp_array = [value] * diff_size
+    new2_array = array + temp_array
+  end
+  new2_array
 end
-
 
 # 3. Refactored Solution
 
